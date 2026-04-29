8e2b02a5e236547ce806ca4535b4dc9666b30c13
Keenan-T7 <Keenan-T7@ulster.ac.uk>
2026-04-29 20:28:04 +0100

    Assignment files being uploaded

---
+++ untitled.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import geopandas as gpd
+import folium
+
+LGD2012 = gpd.read_file('data_files/LGD2012.shp')
+
+df = pd.read_csv('data_files/emergencydepartments.csv')
+
+eds = gpd.GeoDataFrame(df[['Name','Address','Town']],
+                       geometry=gpd.points_from_xy(df['X'],df['Y']),
+                       crs='epsg:29903')
+
+eds.explore('Name',
+            m=m,
+            marker_type='marker',
+            popup=True,
+            legend=False,
+            )
+
+travel = pd.read_csv('data_files/LGD2012.csv')
+
+merged = LGD2012.merge(travel, left_on='LGDCode', right_on='LGDCode')
+
+m = merged.explore('RouteLength_km',
+                   cmap='plasma',
+                   legend_kwds={'caption': 'Distance to nearest Emergency Department (km)'}
+                  )
+
+eds_args = {
+    'm': m,
+    'marker_type': 'marker',
+    'popup': True,
+    'legend': False,
+    'marker_kwds': {'icon': folium.Icon(color='blue', icon='square-h', prefix='fa')}
+}
+
+eds.explore('Name', **eds_args)
+
+m
+
+m.save('NI_EmgDept_test.html')
